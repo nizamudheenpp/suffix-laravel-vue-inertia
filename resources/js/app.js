b746ab6a64e5037e30c269ddb3aee2359aeec3ea
nizamudheen <nizamudheen@lilacinfotech.com>
2025-01-08 18:03:11 +0530

    assets added

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,8 @@
 import '../css/app.css';
 import './bootstrap';
+import '../admin_assets/js/app.js';
+import '../admin_assets/js/app-backup';
+
 
 import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
